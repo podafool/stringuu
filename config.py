fb74e46e653c01454c7e10862fb7d10a19c3b6a1
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-04-15 18:46:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Idhayann")
+OWNER_LINK = getenv("OWNER_LINK", "https://t.me/Idhayann")
